1a3c56950140ec10660c7541f421cc821d6f0e21
Vladimir Serov <me@cab404.ru>
2021-09-10 08:30:03 +0300

    and fadeout. and fixed paths.

--- clock.sh
+++ clock.sh
@@ -2,6 +2,7 @@
 # the simplest time announcer
 
 while true; do 
-    ffmpeg -i ~/test/matrix/downloads/17red-a-b3-c$(date '+%H%M')-d-e3-f-g3-h3-i.oga -loglevel 20 -f wav - | aplay; 
+    date
+    ffmpeg -i ./downloads/17red-a-b3-c$(date '+%H%M')-d-e3-f-g3-h3-i.oga -loglevel 20 -filter_complex 'afade=t=out:st=5:d=1:curve=hsin' -f wav - | aplay; 
     sleep 15m; 
 done
